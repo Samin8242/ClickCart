3e1f6dd42a3b295c365f3280a5d8b125fa99d31f
Samin8242 <106053589+Samin8242@users.noreply.github.com>
2023-05-13 15:09:22 +0600

    repos added

--- ClickCart/DAL/Repos/ShipperRepo.cs
+++ ClickCart/DAL/Repos/ShipperRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.Interfaces;
+using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    internal class ShipperRepo
+    internal class ShipperRepo : Repo, IRepo<Shipper, string, Shipper>
     {
+        public Shipper Create(Shipper obj)
+        {
+            db.Shippers.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+
+        public bool Delete(string id)
+        {
+            var ex = Read(id);
+            db.Shippers.Remove(ex);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Shipper> Read()
+        {
+            return db.Shippers.ToList();
+        }
+
+        public Shipper Read(string id)
+        {
+            return db.Shippers.Find(id);
+        }
+
+        public Shipper Update(Shipper obj)
+        {
+            var ex = Read(obj.Username);
+            db.Entry(ex).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
     }
 }
